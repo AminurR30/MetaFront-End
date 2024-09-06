a16d8b6958323d33229a1904510d4e402bb78f10
Aminur Rahman <150610324+AminurR30@users.noreply.github.com>
2024-09-06 23:32:22 +0530

    Module 1 codes

---
+++ Course 2/Module 1&2/e4.js
@@ -0,0 +1,29 @@
+for (var i = 1; i <= 5; i++) {
+  console.log(i);
+}
+console.log("Counting completed!");
+
+for (var i = 5; i >= 1; i--) {
+  console.log(i);
+}
+console.log("Countdown finished!");
+
+var c = 1;
+while (c <= 5) {
+  console.log(c);
+  c++;
+}
+console.log("Counting completed!");
+
+var i = 5;
+while (i > 0) {
+  console.log(i);
+  i--;
+}
+console.log("Countdown finished!");
+
+var year = 2018;
+while (year < 2023) {
+  console.log(year);
+  year++;
+}
